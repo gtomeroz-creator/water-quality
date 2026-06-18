ffc269be24e9027f351c95422794af9de9efd124
gtomeroz-creator <gtomeroz@gmail.com>
2026-06-18 15:36:54 +0300

    Update cache URLs in service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
 const CACHE_NAME = 'water-quality-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+    './',
+    './index.html'
 ];
 
 // Install event
